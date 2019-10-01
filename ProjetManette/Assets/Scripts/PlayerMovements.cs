73de64e19b0d6137b84ac04fdc87150bb8d7cbac
AlgO <alexandre.brethes@gmail.com>
2019-10-01 17:12:31 +0200

    PlayerMovements basis

--- ProjetManette/Assets/Scripts/PlayerMovements.cs
+++ ProjetManette/Assets/Scripts/PlayerMovements.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Physics _playerBody;
+
+    [SerializeField] private float _jumpForce = 100;
+    [SerializeField] private float _speed = 30;
+
+    private float _jumpBuffer = .15f;
+    [Range(0, .3f)] [SerializeField] float _movementSmoothing = .05f;
+    bool _isDoubleJumping = false;
+
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] Transform _groundCheck1;
+    [SerializeField] Transform _groundCheck2;
+    [SerializeField] float _groundedRadius;
+    bool _isGrounded = false;
+    bool _canDoubleJump = true;
+
+    [SerializeField] Transform _ceilingCheck;
+
+    private void Awake()
     {
-        
+        _playerBody = GetComponent<Physics>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
-        
+        bool wasGrounded = _isGrounded;
+        _isGrounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck1.position, _groundedRadius, _groundLayer);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                _isGrounded = true;
+            }
+        }
+
+        colliders = Physics2D.OverlapCircleAll(_groundCheck2.position, _groundedRadius, _groundLayer);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                _isGrounded = true;
+            }
+        }
+
+        if (_isGrounded)
+        {
+            _canDoubleJump = true;
+        }
+        else
+        {
+            _jumpBuffer -= Time.deltaTime;
+            if (_jumpBuffer > 0)
+            {
+                _isGrounded = true;
+            }
+        }
     }
+
+    public void Move(float horizontalMove, bool jump)
+    {
+        // Horizontal movement
+        Vector2 targetVelocity = new Vector2(horizontalMove * _speed, _playerBody.velocity.y);
+
+        if (jump)
+        {
+            if (_isGrounded)
+            {
+                // Jump
+                targetVelocity.y = _jumpForce;
+            }
+            else if (!_isGrounded && _canDoubleJump)
+            {
+                // Jump
+                targetVelocity.y = _jumpForce;
+                _canDoubleJump = false;
+            }
+            _isGrounded = false;
+        }
+
+        _playerBody.velocity = Vector2.SmoothDamp(_playerBody.velocity, targetVelocity, ref _playerBody.velocity, _movementSmoothing);
+    }
+
 }
